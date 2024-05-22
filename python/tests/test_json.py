0c27c4ac7aa2256bee56508f5e5819b3e3364a88
Mattt Zmuda <mattt@replicate.com>
2024-05-22 11:58:12 -0700

    Fix test_make_encodeable_ignores_files
    Signed-off-by: Mattt Zmuda <mattt@replicate.com>

--- python/tests/test_json.py
+++ python/tests/test_json.py
@@ -3,9 +3,10 @@ import tempfile
 
 import cog
 import numpy as np
+import pydantic
 from cog.files import upload_file
 from cog.json import make_encodeable, upload_files
-from pydantic import BaseModel
+from cog.types import PYDANTIC_V2
 
 
 def test_make_encodeable_recursively_encodes_tuples():
@@ -14,9 +15,12 @@ def test_make_encodeable_recursively_encodes_tuples():
 
 
 def test_make_encodeable_encodes_pydantic_models():
-    class Model(BaseModel):
+    class Model(pydantic.BaseModel):
         text: str
         number: int
+
+        if PYDANTIC_V2:
+            model_config = pydantic.ConfigDict(arbitrary_types_allowed=True)
 
     assert make_encodeable(Model(text="hello", number=5)) == {
         "text": "hello",
@@ -25,7 +29,7 @@ def test_make_encodeable_encodes_pydantic_models():
 
 
 def test_make_encodeable_ignores_files():
-    class Model(BaseModel):
+    class Model(pydantic.BaseModel):
         path: cog.Path
 
     temp_dir = tempfile.mkdtemp()
@@ -49,13 +53,11 @@ def test_upload_files():
 
 
 def test_numpy():
-    class Model(BaseModel):
+    class Model(pydantic.BaseModel):
         ndarray: np.ndarray
         npfloat: np.float64
         npinteger: np.integer
-
-        class Config:
-            arbitrary_types_allowed = True
+        model_config = ConfigDict(arbitrary_types_allowed=True)
 
     model = Model(
         ndarray=np.array([[1, 2], [3, 4]]),
